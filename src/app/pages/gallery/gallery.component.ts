c0aeea9ce27e06c4581576226b361aa80d9a6805
Anirudh Jwala <anirudhsai2@gmail.com>
2021-05-10 14:27:58 +0530

    Gallery view image in large

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-gallery',
@@ -6,7 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.css'],
 })
 export class GalleryComponent implements OnInit {
-  constructor() {}
+  galleryImages: string[];
 
-  ngOnInit(): void {}
+  constructor(public dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    this.galleryImages = [
+      '/assets/gallery/Apark1.jpg',
+      '/assets/gallery/Apark2.jpg',
+      '/assets/gallery/Apark3.jpg',
+      '/assets/gallery/Apark4.jpg',
+      '/assets/gallery/Apark5.jpg',
+      '/assets/gallery/Apark6.jpg',
+      '/assets/gallery/Apark7.jpg',
+      '/assets/gallery/Apark8.jpg',
+    ];
+  }
+
+  openImageDialog(image: string) {
+    this.dialog.open(DialogDataExampleDialog, {
+      width: '60%',
+      data: {
+        imageUrl: image,
+      },
+    });
+  }
+}
+
+@Component({
+  template: `
+    <div mat-dialog-content>
+      <img src="{{ data.imageUrl }}" class="img img-fluid rounded" />
+    </div>
+  `,
+})
+export class DialogDataExampleDialog {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
 }
